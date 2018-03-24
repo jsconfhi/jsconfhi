86161b804ec6e3f4d61492c2cbae98d5b98ab892
Jon Kuperman <jon.kuperman@gmail.com>
2018-03-24 12:49:37 -0700

    style nav component

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,20 +1,30 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from "react";
+import Link from "gatsby-link";
+import styled from "styled-components";
+
+const List = styled.ul`
+  list-style: none;
+  display: flex;
+  justify-content: space-between;
+`;
 
 const Nav = () => (
   <nav>
-    <ul
-      style={{
-        listStyle: 'none',
-        display: 'flex',
-        justifyContent: 'space-between'
-      }}
-    >
-      <Link to="/schedule/" style={{ color: 'white', textDecoration: 'none'}}>Schedule</Link>
-      <Link to="/call-for-speakers/" style={{ color: 'white', textDecoration: 'none'}}>Speak</Link>
-      <Link to="/sponsors/" style={{ color: 'white', textDecoration: 'none'}}>Sponsors</Link>
-    </ul>
+    <List>
+      <Link to="/schedule/" style={{ color: "white", textDecoration: "none" }}>
+        Schedule
+      </Link>
+      <Link
+        to="/call-for-speakers/"
+        style={{ color: "white", textDecoration: "none" }}
+      >
+        Speak
+      </Link>
+      <Link to="/sponsors/" style={{ color: "white", textDecoration: "none" }}>
+        Sponsors
+      </Link>
+    </List>
   </nav>
-)
+);
 
-export default Nav
+export default Nav;
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,17 +1,26 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React from "react";
+import PropTypes from "prop-types";
+import Helmet from "react-helmet";
+import { injectGlobal } from "styled-components";
+import reset from "styled-reset";
 
-import Header from '../components/Header'
-import './index.css'
+import Header from "../components/Header";
+
+injectGlobal`
+  ${reset}
+  body {
+    font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif; 
+   font-weight: 300;
+  }
+`;
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
       title="Gatsby Default Starter"
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: "description", content: "Sample" },
+        { name: "keywords", content: "sample, something" }
       ]}
     >
       <html lang="en" />
@@ -19,19 +28,19 @@ const TemplateWrapper = ({ children }) => (
     <Header />
     <div
       style={{
-        margin: '0 auto',
+        margin: "0 auto",
         maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
+        padding: "0px 1.0875rem 1.45rem",
+        paddingTop: 0
       }}
     >
       {children()}
     </div>
   </div>
-)
+);
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func,
-}
+  children: PropTypes.func
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;

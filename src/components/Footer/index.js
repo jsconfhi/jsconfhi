785225030a4f2dcc5bc70f16de987689fd821ae5
Charlie Croom <ccroom@twitter.com>
2018-07-04 10:10:56 -0700

    Update Footer; Add SVG FA icons; Meta tags

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,28 +2,51 @@ import React from 'react'
 import Nav from '../Nav'
 import Link from 'gatsby-link'
 import styled from 'styled-components'
-import twitterLogo from '../../img/twitter-logo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faInstagram, faTwitterSquare } from '@fortawesome/free-brands-svg-icons'
+
+
+export const footerHeight = 40;
 
 const FooterWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0;
+  padding: 0 1em;
+  color: #03282c;
+  background: #f2dc1a;
+  flex-shrink: 1;
+  height: ${footerHeight}px;
+  font-size: 0.9em;
+  font-weight: bold;
   width: 100%;
-  background: #00bcd4;
-  height: 70px;
-  margin-top: 2em;
+  position: fixed;
+  bottom: 0;
 `
 
-const FooterContent = styled.ul`
-  height: 70px;
-  max-width: 960px;
-  margin: 0 auto;
+const FooterLinks = styled.ul`
   display: flex;
-  justify-content: space-around;
-  align-items: center;
+  flex-grow: 1;
+  list-style: none;
+`
+
+const FooterSocials = styled.div`
+  font-size: 2em;
 `
 
 const FooterLink = styled.a`
-  color: black;
-  font-size: 0.8em;
-  font-weight: 500;
+  color: #03282c;
+  padding: 0 1em;
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
+`
+
+const FooterSocialLink = styled.a`
+  color: #03282c;
+  padding: 0 0.3em;
   text-decoration: none;
   &:hover {
     text-decoration: underline;
@@ -32,26 +55,27 @@ const FooterLink = styled.a`
 
 const Footer = () => (
   <FooterWrapper>
-    <FooterContent>
-      <li>
-        <FooterLink href="https://www.twitter.com/jsconfhi" target="_blank">
-          <img width="30" height="30" alt="Twitter" src={twitterLogo} />
-        </FooterLink>
-      </li>
-      <li>
-        <FooterLink href="https://www.twitter.com/jsconfhi" target="_blank">
-          News
-        </FooterLink>
-      </li>
-      <li>
-        <FooterLink href="/organizers">Organizers</FooterLink>
-      </li>
+    <FooterLinks>
       <li>
         <FooterLink href="http://jsconf.com/codeofconduct.html" target="_blank">
           Code of Conduct
         </FooterLink>
       </li>
-    </FooterContent>
+      <li>
+        <FooterLink href="/organizers">Organizers</FooterLink>
+      </li>
+    </FooterLinks>
+    <FooterSocials>
+      <FooterSocialLink aria-label="Instagram" href="https://www.instagram.com/jsconfhi" target="_blank">
+        <FontAwesomeIcon icon={faInstagram} />
+      </FooterSocialLink>
+      <FooterSocialLink aria-label="Twitter" href="https://www.twitter.com/jsconfhi" target="_blank">
+        <FontAwesomeIcon icon={faTwitterSquare} />
+      </FooterSocialLink>
+      <FooterSocialLink aria-label="Facebook" href="https://www.facebook.com/JSConfHi" target="_blank">
+        <FontAwesomeIcon icon={faFacebook} />
+      </FooterSocialLink>
+    </FooterSocials>
   </FooterWrapper>
 )
 
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -6,17 +6,25 @@ import reset from 'styled-reset'
 import styled from 'styled-components'
 
 import Header from '../components/Header'
-import Footer from '../components/Footer'
+import Footer, { footerHeight } from '../components/Footer'
 injectGlobal`
   ${reset}
   body {
-    font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif; 
+    font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif;
     font-weight: 300;
-    font-size: 18px;
+    font-size: .9rem;
+    box-sizing: border-box;
   }
   a { color: #038091; }
   p { line-height: 2em; margin: 1em 0;}
   h1, h2 { margin: 1.5em 0; font-size: 1.5em;  color: #038091; }
+  *,
+  *::after,
+  *::before {
+    margin: 0;
+    padding: 0;
+    box-sizing: inherit;
+  }
 `
 
 const Wrapper = styled.div`
@@ -25,23 +33,38 @@ const Wrapper = styled.div`
   height: 100vh;
 }`
 
+const MainContent = styled.div`
+  padding-bottom: ${footerHeight}px;
+`
+
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="JSConf Hawaii"
+      title="JSConf: Hawaiʻi"
       meta={[
-        { name: 'description', content: 'JS Conf Hawaii' },
+        { name: 'description', content: 'A two day, single track conference dedicated to web development, JavaScript and building a community. February 2019 in Honolulu.' },
         {
           name: 'keywords',
           content: 'JS, JavaScript, Conference, Hawaii, Tech',
         },
+        { name: 'theme-color', content: '#03282c'},
+        { name: 'viewport', content: 'width=device-width, initial-scale=1'},
+        { name: 'twitter:card', content: 'summary'},
+        { name: 'twitter:site', content: '@JSConfHi'},
+        { name: 'og:title', content: 'JSConf: Hawaiʻi'},
+        { name: 'og:description', content: 'A two day, single track conference dedicated to web development, JavaScript and building a community. February 2019 in Honolulu.'},
+        { name: 'og:image', content: 'https://www.jsconfhi.com/android-chrome-512x512.png'}
       ]}
     >
       <html lang="en" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="192x192" href="/favicon-192x192.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
     </Helmet>
     <Wrapper>
       <Header style={{ flex: 1 }} />
-      <div style={{ flex: 1, flexGrow: 1 }}>{children()}</div>
+      <MainContent style={{ flex: 1, flexGrow: 1 }}>{children()}</MainContent>
       <Footer style={{ flex: 1 }} />
     </Wrapper>
   </div>

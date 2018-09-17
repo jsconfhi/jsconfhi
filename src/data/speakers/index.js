6f64c388c278f3f90e0b124bebfe44345d037494
Charlie Croom <ccroom@twitter.com>
2018-09-17 10:12:44 -0700

    Second batch of speakers

--- src/data/speakers/index.js
+++ src/data/speakers/index.js
@@ -1,5 +1,10 @@
+import angAvatar from '../speakers/avatars/Trish_Ang.jpg';
+import clarkAvatar from '../speakers/avatars/Lin_Clark.jpg';
 import cuervoAvatar from '../speakers/avatars/Andres_Cuervo.png';
 import hallidayAvatar from '../speakers/avatars/James_Halliday.jpg';
+import martinsAvatar from '../speakers/avatars/Willian_Martins.jpg';
+import pitiyanuvathAvatar from '../speakers/avatars/Visnu_Pitiyanuvath.jpg';
+import schneidereitAvatar from '../speakers/avatars/Till_Schneidereit.jpg';
 import vlaevaAvatar from '../speakers/avatars/Stanimira_Vlaeva.jpg';
 import wibowoAvatar from '../speakers/avatars/Amy_Wibowo.jpg';
 
@@ -41,5 +46,40 @@ export default [
     name: 'Stanimira Vlaeva',
     talk: 'Embedding V8 in the real world',
     twitter: 'StanimiraVlaeva'
-  }
+  },
+  {
+    avatar: clarkAvatar,
+    bio: `Lin Clark makes code cartoons. She's also an engineer in Mozilla’s Emerging Technologies group, where she works with the WebAssembly and Rust teams. In previous lives, she worked at npm, was a core contributor to open source projects like Firefox’s developer tools, and contributed to HTML data standards.`,
+    name: "Lin Clark",
+    talk: "A Cartoon Quest: New Adventures for WebAssembly",
+    twitter: "linclark"
+  },
+  {
+    avatar: schneidereitAvatar,
+    bio: `Till Schneidereit directs the WebAssembly tooling efforts at Mozilla. His team is responsible for Emscripten, the compiler toolchain that kickstarted asm.js and subsequently WebAssembly; coordinating the Rust-to-WebAssembly compilation efforts; and other projects around WebAssembly development and deployment. He’s also a delegate to the language standardization bodies for both JavaScript and WebAssembly.`,
+    name: "Till Schneidereit",
+    talk: "A Cartoon Quest: New Adventures for WebAssembly",
+    twitter: "tschneidereit"
+  },
+  {
+    avatar: angAvatar,
+    bio: `Designer slash developer, climber of rocks, boarder of snow. Currently wrangling code on the Platform team at Slack. Also a morning person and host of CreativeMornings/Oakland.`,
+    name: "Trish Ang",
+    talk: "How to Fail at Accessibility",
+    twitter: "feesh"
+  },
+  {
+    avatar: martinsAvatar,
+    bio: `JS formatter/CSS tweaker @eBay. From São Paulo Brazil, but lives in Berlin. Sim racer gamer and Soccer fan.`,
+    name: "Willian Martins",
+    talk: "Back to the future of JS II: Beyond what we can foresee",
+    twitter: "wmsbill"
+  },
+  {
+    avatar: pitiyanuvathAvatar,
+    bio: "Visnu has dropped two production databases in his computering career. He currently works at Opendoor on analytics and data visualization. He is a co-organizer of WaffleJS in San Francisco and if you're ever bored and would like to see his face, run `npm visnup`.",
+    name: "Visnu Pitiyanuvath",
+    talk: "How to cheat at life, with JavaScript, computer vision, and augmented reality",
+    twitter: "visnup"
+  },
 ];

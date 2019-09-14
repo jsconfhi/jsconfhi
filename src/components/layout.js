9369aa44b9a15ec1e8a53489590b2f99f15db73d
Jon Kuperman <kuperman@adobe.com>
2019-09-14 15:36:15 -0700

    updates

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Helmet from 'react-helmet';
-import { injectGlobal } from 'styled-components';
-import reset from 'styled-reset';
-import styled from 'styled-components';
-import theme from '../theme';
+import React from "react";
+import PropTypes from "prop-types";
+import Helmet from "react-helmet";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import styled from "styled-components";
+import theme from "../theme";
 
-import Header, { headerHeight } from '../components/Header';
-import Footer, { footerHeight } from '../components/Footer';
-injectGlobal`
+import Header, { headerHeight } from "../components/Header";
+import Footer, { footerHeight } from "../components/Footer";
+const GlobalStyles = createGlobalStyle`
   ${reset}
   body {
     background-color: ${theme.colors.background};
@@ -41,66 +41,69 @@ const MainContent = styled.div`
 `;
 
 const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet
-      title="JSConf Hawaiʻi"
-      meta={[
-        {
-          name: 'description',
-          content:
-            'A two day, single track conference dedicated to web development, JavaScript and building a community. February 7th + 8th 2019 in Honolulu, Hawaiʻi.'
-        },
-        {
-          name: 'keywords',
-          content: 'JS, JavaScript, Conference, Hawaii, Tech, Web'
-        },
-        { name: 'theme-color', content: '#03282c' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'twitter:card', content: 'summary' },
-        { name: 'twitter:site', content: '@JSConfHi' },
-        { name: 'og:title', content: 'JSConf Hawaiʻi' },
-        {
-          name: 'og:description',
-          content:
-            'A two day, single track conference dedicated to web development, JavaScript and building a community. February 7th + 8th 2019 in Honolulu, Hawaiʻi.'
-        },
-        {
-          name: 'og:image',
-          content: 'https://www.jsconfhi.com/android-chrome-512x512.png'
-        }
-      ]}
-    >
-      <html lang="en" />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="192x192"
-        href="/favicon-192x192.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
-    </Helmet>
-    <Wrapper>
-      <Header style={{ flex: 1 }} />
-      <MainContent>{children}</MainContent>
-      <Footer style={{ flex: 1 }} />
-    </Wrapper>
-  </div>
+  <>
+    <GlobalStyles />
+    <div>
+      <Helmet
+        title="JSConf Hawaiʻi"
+        meta={[
+          {
+            name: "description",
+            content:
+              "A two day, single track conference dedicated to web development, JavaScript and building a community. February 7th + 8th 2019 in Honolulu, Hawaiʻi."
+          },
+          {
+            name: "keywords",
+            content: "JS, JavaScript, Conference, Hawaii, Tech, Web"
+          },
+          { name: "theme-color", content: "#03282c" },
+          { name: "viewport", content: "width=device-width, initial-scale=1" },
+          { name: "twitter:card", content: "summary" },
+          { name: "twitter:site", content: "@JSConfHi" },
+          { name: "og:title", content: "JSConf Hawaiʻi" },
+          {
+            name: "og:description",
+            content:
+              "A two day, single track conference dedicated to web development, JavaScript and building a community. February 7th + 8th 2019 in Honolulu, Hawaiʻi."
+          },
+          {
+            name: "og:image",
+            content: "https://www.jsconfhi.com/android-chrome-512x512.png"
+          }
+        ]}
+      >
+        <html lang="en" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/favicon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+      </Helmet>
+      <Wrapper>
+        <Header style={{ flex: 1 }} />
+        <MainContent>{children}</MainContent>
+        <Footer style={{ flex: 1 }} />
+      </Wrapper>
+    </div>
+  </>
 );
 
 TemplateWrapper.propTypes = {

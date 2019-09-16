caa488a9620cc31958dc373129300f0dc69cb993
Jon Kuperman <jon.kuperman@gmail.com>
2019-09-15 22:20:03 -0700

    got header working

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import LogoSVG from "../img/logo.svg";
 import styled from "styled-components";
+import { FaBars } from "react-icons/fa";
 
 const SkipLink = styled.a`
   position: absolute;
@@ -22,50 +23,81 @@ const SkipLink = styled.a`
   }
 `;
 
-const Header = () => (
-  <header>
-    <SkipLink href="#content">Skip to content</SkipLink>
-    <div className="content">
-      <div className="grid">
-        <div className="grid-full">
-          <nav className="nav-top">
-            <ul className="nav-left">
-              <li>
-                <a className="home" href="/">
-                  <LogoSVG className="logo" />
-                  <div className="home-text">JSConf Hawaiʻi</div>
-                </a>
-              </li>
-              <li className="nav-hamburger">
-                <button href="" aria-label="More" className="icon">
-                  <i className="fas fa-bars" />
-                </button>
-              </li>
-            </ul>
-            <ul className="nav-right">
-              <li>
-                <a href="/attend">Attend</a>
-              </li>
-              <li>
-                <a href="/speakers">Speakers</a>
-              </li>
-              <li>
-                <a href="/sponsors">Sponsors</a>
-              </li>
-              <li>
-                <a
-                  className="buy-button button-primary"
-                  href="https://ti.to/jsconf-hawaii/2020/en"
-                >
-                  Buy tickets
-                </a>
-              </li>
-            </ul>
-          </nav>
+const Header = () => {
+  const [state, setState] = useState(false);
+
+  return (
+    <header>
+      <SkipLink href="#content">Skip to content</SkipLink>
+      <div className="content">
+        <div className="grid">
+          <div className="grid-full">
+            <nav className="nav-top">
+              <ul className="nav-left">
+                <li>
+                  <a className="home" href="/">
+                    <LogoSVG className="logo" />
+                    <div className="home-text">JSConf Hawaiʻi</div>
+                  </a>
+                </li>
+                <li className="nav-hamburger">
+                  <button
+                    aria-label="More"
+                    className="icon"
+                    onClick={() => (state ? setState(false) : setState(true))}
+                  >
+                    <FaBars />
+                  </button>
+                </li>
+              </ul>
+              <ul className="nav-right">
+                <li>
+                  <a href="/attend">Attend</a>
+                </li>
+                <li>
+                  <a href="/speakers">Speakers</a>
+                </li>
+                <li>
+                  <a href="/sponsors">Sponsors</a>
+                </li>
+                <li>
+                  <a
+                    className="buy-button button-primary"
+                    href="https://ti.to/jsconf-hawaii/2020/en"
+                  >
+                    Buy tickets
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
       </div>
-    </div>
-  </header>
-);
+      {state && (
+        <div className="nav-expandable">
+          <ul className="nav-right-expandable">
+            <li>
+              <a href="/attend">Attend</a>
+            </li>
+            <li>
+              <a href="/#speakers">Speakers</a>
+            </li>
+            <li>
+              <a href="/sponsors">Sponsors</a>
+            </li>
+            <li>
+              <a
+                className="buy-button button-primary"
+                href="https://ti.to/jsconf-hawaii/2020/en"
+              >
+                Buy tickets
+              </a>
+            </li>
+          </ul>
+        </div>
+      )}
+    </header>
+  );
+};
 
 export default Header;

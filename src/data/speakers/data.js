050d269743d89e521cb3d10ea15f210dfbec5f54
Kelly King <kking@twitter.com>
2019-11-20 13:21:33 -0800

    Add Alex and Lindsey to speakers

--- src/data/speakers/data.js
+++ src/data/speakers/data.js
@@ -1,9 +1,10 @@
 import JennaImage from "../../img/JennaZeigen.jpeg";
 import DineshImage from "../../img/dinesh.jpeg";
 import AngieImage from "../../img/angie.jpeg"
+import AlexImage from "../../img/alex.jpeg"
+import LindseyImage from "../../img/lindsey.jpeg"
 
-export default [
-  {
+export default [{
     name: 'Jenna Ziegen',
     handle: 'zeigenvector',
     img: JennaImage,
@@ -25,10 +26,18 @@ export default [
     description: 'Automation has come a long way in assisting with regression testing efforts. Teams worldwide are successfully running hundreds of functional regression tests at every check-in. While this provides a great source of confidence, critical regression bugs are still missed using this approach. That’s because these tests can only assert on what their human programmer asks them to. Additional errors with functionality, UX, and usability often go uncaught using today’s most common test automation techniques. For this reason, the top companies in all sectors of the industry are turning to visual validation. Visual validation is a relatively new concept that can be used to enhance existing automated tests and provide an easy way to perform those difficult checks for things like UX, localization, usability, responsive design, and cross-device testing. In this talk, you’ll learn how visual validation works, see an integration into an existing test code base, and discuss the pros and cons of using various visual validation techniques.'
   },
   {
-    name: 'Placeholder'
+    name: 'Alex Sexton',
+    handle: 'SlexAxton',
+    img: AlexImage,
+    title: 'Advanced Design System Considerations',
+    description: `"Design Systems" and component libraries are a popular solution for abstracting common UI elements and flows to be reusable across many applications.They allow you to spend more time on the details, and reduce the complexity of implementing consistent experiences.There's a good chance that you're using one already! Scaling these systems to provide value to *lots* of teams across your organization, or in open - source - land, requires solving for each team's different use-cases simultaneously. How do you add in support for internationalization, accessibility, theming (dark mode!), server rendering, or multiple render targets? How do you support the teams that need it in React *and* the teams that need it in Svelte? How do you provide a system that is flexible enough, but doesn't become prohibitively complex? How can you export your components to popular design tools for your designers to build with? Let's dive into approaches to solving these problems in scalable ways, and take a look at how Stripe has chosen to tackle some of these issues in the real world. Let's look at how to make sure a design system specifies design choices and allows users to build quickly, but doesn't lock you into specific frameworks or trendy tools out of the gate.`
   },
   {
-    name: 'Placeholder'
+    name: 'Lindsey Dragun ',
+    handle: 'techevangelista',
+    img: LindseyImage,
+    title: 'Impactful Accessibility - The Changes That Mean The Most',
+    description: "The sad truth is that even companies that wish to have accessible websites often only dedicate the time and effort to create SOME accessibility. What they choose to make accessible is often based on what a handful of people, often abled bodied ones, decide, and doesn’t always reflect what users want or need. Using a series of surveys and discussions with other disabled people, I have synthesized suggestions and examples of what makes the most impact for many users or gigantic impacts on usage for smaller groups.We'll go over the look, feel, and functionality of websites and some basic design changes that can be used to great benefit."
   },
   {
     name: 'Placeholder'

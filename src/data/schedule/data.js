6276b36b946d2fadaade8d3acbd7a34738791055
Jon Kuperman <jon.kuperman@gmail.com>
2020-01-19 22:35:22 -0500

    update schedule

--- src/data/schedule/data.js
+++ src/data/schedule/data.js
@@ -1,113 +1,117 @@
 export const day0 = [
   {
-    time: '12-6pm',
-    description: 'Check-in at Alohilani lobby (optional)'
+    time: "2-5pm",
+    description: "Check-in at Alohilani lobby (optional)"
   },
   {
-    time: '6-9pm',
-    description: 'Dinner roulette* (optional)'
+    time: "6-9pm",
+    description: "Dinner roulette* (optional)"
+  },
+  {
+    time: "9-10pm",
+    description: "Check-in at Alohilani lobby (optional)"
   }
 ];
 export const day1 = [
   {
-    time: '8:30am',
-    description: 'Breakfast & check-in',
+    time: "8:30am",
+    description: "Breakfast & check-in",
     highlight: true
   },
-  { time: '9am', speakerId: 6 },
-  { time: '9:30am', speakerId: 12 },
-  { time: '10am', speakerId: 20 },
+  { time: "9am", speakerId: 6 },
+  { time: "9:30am", speakerId: 12 },
+  { time: "10am", speakerId: 20 },
   {
-    time: '10:30am',
-    description: 'Snack break',
+    time: "10:30am",
+    description: "Snack break",
     highlight: true
   },
-  { time: '11am', speakerId: 1 },
-  { time: '11:30', speakerId: 21 },
+  { time: "11am", speakerId: 1 },
+  { time: "11:30", speakerId: 21 },
   {
-    time: '12pm',
-    description: 'Lunch',
+    time: "12pm",
+    description: "Lunch",
     highlight: true
   },
-  { time: '1pm', speakerId: 9 },
-  { time: '1:30pm', speakerId: 22 },
-  { time: '2pm', speakerId: 18 },
+  { time: "1pm", speakerId: 9 },
+  { time: "1:30pm", speakerId: 22 },
+  { time: "2pm", speakerId: 18 },
   {
-    time: '2:30pm',
-    description: 'Snack break',
+    time: "2:30pm",
+    description: "Snack break",
     highlight: true
   },
-  { time: '3pm', speakerId: 19 },
-  { time: '3:30pm', speakerId: 5 },
-  { time: '4pm', speakerId: 10 },
+  { time: "3pm", speakerId: 19 },
+  { time: "3:30pm", speakerId: 5 },
+  { time: "4pm", speakerId: 10 },
   {
-    time: '6-9pm',
-    description: 'After-party at the Honolulu Aquarium',
+    time: "6-9pm",
+    description: "After-party at the Honolulu Aquarium",
     highlight: true
   }
 ];
 
 export const day2 = [
   {
-    time: '9am-12pm',
-    description: 'Guided hike at Manoa Falls'
+    time: "9am-12pm",
+    description: "Guided hike at Manoa Falls"
   },
   {
-    time: '10am-3pm',
-    description: 'Workshop: Web Performance by Foo Bar'
+    time: "10am-3pm",
+    description: "Workshop: Web Performance by Foo Bar"
   },
   {
-    time: '10am-3pm',
-    description: 'Workshop: Advanced React Patterns by Foo Bar'
+    time: "10am-3pm",
+    description: "Workshop: Advanced React Patterns by Foo Bar"
   },
   {
-    time: '11am-2pm',
-    description: 'Snorkel trip to Hanauma Bay'
+    time: "11am-2pm",
+    description: "Snorkel trip to Hanauma Bay"
   },
   {
-    time: '1-4pm',
-    description: 'Lunch cruise at Something Pier'
+    time: "1-4pm",
+    description: "Lunch cruise at Something Pier"
   },
   {
-    time: '2-4pm',
-    description: 'Surf lesson on Waikiki Beach'
+    time: "2-4pm",
+    description: "Surf lesson on Waikiki Beach"
   }
 ];
 export const day3 = [
   {
-    time: '8:30am',
-    description: 'Breakfast & check-in',
+    time: "8:30am",
+    description: "Breakfast & check-in",
     highlight: true
   },
-  { time: '9am', speakerId: 14 },
-  { time: '9:30am', speakerId: 4 },
-  { time: '10am', speakerId: 17 },
+  { time: "9am", speakerId: 14 },
+  { time: "9:30am", speakerId: 4 },
+  { time: "10am", speakerId: 17 },
   {
-    time: '10:30am',
-    description: 'Snack break',
+    time: "10:30am",
+    description: "Snack break",
     highlight: true
   },
-  { time: '11am', speakerId: 3 },
-  { time: '11:30', speakerId: 13 },
+  { time: "11am", speakerId: 3 },
+  { time: "11:30", speakerId: 13 },
   {
-    time: '12pm',
-    description: 'Lunch',
+    time: "12pm",
+    description: "Lunch",
     highlight: true
   },
-  { time: '1pm', speakerId: 15 },
-  { time: '1:30pm', speakerId: 11 },
-  { time: '2pm', speakerId: 7 },
+  { time: "1pm", speakerId: 15 },
+  { time: "1:30pm", speakerId: 11 },
+  { time: "2pm", speakerId: 7 },
   {
-    time: '2:30pm',
-    description: 'Snack break',
+    time: "2:30pm",
+    description: "Snack break",
     highlight: true
   },
-  { time: '3pm', speakerId: 8 },
-  { time: '3:30pm', speakerId: 16 },
-  { time: '4pm', speakerId: 2 },
+  { time: "3pm", speakerId: 8 },
+  { time: "3:30pm", speakerId: 16 },
+  { time: "4pm", speakerId: 2 },
   {
-    time: '5pm',
-    description: 'Group photo and goodbye drinks at the Alohilani pool*',
+    time: "5pm",
+    description: "Group photo and goodbye drinks at the Alohilani pool*",
     highlight: true
   }
 ];
